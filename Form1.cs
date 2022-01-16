dd1759366cc9035f1b0528727f7b3879dd734985
Robert <yoyorobertionut@gmail.com>
2022-01-16 19:43:25 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+using OpenTK.Graphics.OpenGL;
+
+using OpenTK3_StandardTemplate_WinForms.helpers;
+using OpenTK3_StandardTemplate_WinForms.objects;
+
+namespace OpenTK3_StandardTemplate_WinForms
+{
+    public partial class MainForm : Form
+    {
+        private Axes mainAxis;
+        private Rectangles re;
+        private Camera cam;
+        private Scene scene;
+        private Cube cubuletz;
+
+        private Point mousePosition;
+
+        public MainForm()
+        {   
+            // general init
+            InitializeComponent();
+
+            // init VIEWPORT
+            scene = new Scene();
+
+            scene.GetViewport().Load += new EventHandler(this.mainViewport_Load);
+            scene.GetViewport().Paint += new PaintEventHandler(this.mainViewport_Paint);
+            scene.GetViewport().MouseMove += new MouseEventHandler(this.mainViewport_MouseMove);
+
+            this.Controls.Add(scene.GetViewport());
+           
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+            // init RNG
+            Randomizer.Init();
+
+            // init CAMERA/EYE
+            cam = new Camera(scene.GetViewport());
+
+            // init AXES
+            mainAxis = new Axes(showAxes.Checked);
+            // re = new Rectangles();
+            cubuletz = new Cube();
+            cubuletz.Draw();
+
+        }
+
+        private void showAxes_CheckedChanged(object sender, EventArgs e)
+        {
+            mainAxis.SetVisibility(showAxes.Checked);
+
+            scene.Invalidate();
+        }
+
+        private void changeBackground_Click(object sender, EventArgs e)
+        {
+            GL.ClearColor(Randomizer.GetRandomColor());
+
+            scene.Invalidate();
+        }
+
+        private void resetScene_Click(object sender, EventArgs e)
+        {
+            showAxes.Checked = true;
+            mainAxis.SetVisibility(showAxes.Checked);
+            scene.Reset();
+            cam.Reset();
+
+            scene.Invalidate();
+        }
+
+        private void mainViewport_Load(object sender, EventArgs e)
+        {
+            scene.Reset();
+        }
+
+        private void mainViewport_MouseMove(object sender, MouseEventArgs e)
+        {
+            mousePosition = new Point(e.X, e.Y);
+            scene.Invalidate();
+        }
+
+        private void mainViewport_Paint(object sender, PaintEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.DepthBufferBit);
+
+            cam.SetView();
+
+            if (enableRotation.Checked == true)
+            {
+                // Doar după axa Ox.
+                GL.Rotate(Math.Max(mousePosition.X, mousePosition.Y), 1, 1, 1);
+            }
+
+            // GRAPHICS PAYLOAD
+            mainAxis.Draw();
+
+            if (enableObjectRotation.Checked == true)
+            {
+                // Rotatie a obiectului
+                GL.PushMatrix();
+                GL.Rotate(Math.Max(mousePosition.X, mousePosition.Y), 1, 1, 1);
+                // re.Draw();
+                cubuletz.Draw();
+                GL.PopMatrix();
+            } else
+            {
+                //re.Draw();
+                cubuletz.Draw();
+                
+            }
+
+            scene.GetViewport().SwapBuffers();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            cubuletz.ToggleVisibility();
+            scene.Invalidate();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            showAxes.Checked = true;
+            mainAxis.SetVisibility(showAxes.Checked);
+            scene.Reset();
+            cam.Reset();
+            cubuletz.Hide();
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+
+            scene.Invalidate();
+        }
+    }
+}
---
+++ objects/Cube.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Drawing;
+
+using OpenTK.Graphics.OpenGL;
+
+using OpenTK3_StandardTemplate_WinForms.helpers;
+namespace OpenTK3_StandardTemplate_WinForms.objects
+{
+    class Cube
+    {
+        private ArrayList coordonates;
+        private ArrayList colors;
+        private PolygonMode currentPolygonState = PolygonMode.Fill;
+        private bool visibility;
+        private int Translate = 20;
+
+        public Cube()
+        {
+            coordonates = new ArrayList();
+            colors = new ArrayList();
+            Culoare();
+            ReadFromFile();
+            visibility = false;
+        }
+        public void ReadFromFile()
+        {
+            String[] str = File.ReadAllLines(@"C:\Users\Robert\Downloads\test\OpenTK3_StandardTemplate_WinForms\helpers\TextFile1.txt");
+            foreach(String strFile in str)
+            {
+                String[] file;
+                file = strFile.Split(' ');
+                coordonates.Add(new Coords(Convert.ToInt32(file[0])+Translate, Convert.ToInt32(file[1])+Translate,Convert.ToInt32(file[2])+Translate));
+            }
+        }
+        public void Culoare()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                colors.Add(Randomizer.GetRandomColor());
+            }
+        }
+
+        public bool GetVisibility()
+        {
+            return visibility;
+        }
+
+        public void SetVisibility(bool _visibility)
+        {
+            visibility = _visibility;
+        }
+
+        public void Show()
+        {
+            visibility = true;
+        }
+
+        public void Hide()
+        {
+            visibility = false;
+        }
+
+        public void ToggleVisibility()
+        {
+            visibility = !visibility;
+        }
+
+        public void PolygonDrawingStyle(String style)
+        {
+            if (style == "line")
+            {
+                currentPolygonState = PolygonMode.Line;
+                return;
+            }
+
+            if (style == "surface")
+            {
+                currentPolygonState = PolygonMode.Fill;
+                return;
+            }
+
+        }
+
+        public void Draw()
+        {
+            if (!visibility)
+            {
+                return;
+            }
+            int j = 0;
+            GL.PolygonMode(MaterialFace.FrontAndBack, currentPolygonState);
+            GL.Begin(PrimitiveType.Quads);
+            for (int i = 0; i < 6; i++)
+            {
+                GL.Color3((Color)colors[i]);
+                GL.Vertex3(((Coords)coordonates[j]).X, ((Coords)coordonates[j]).Y, ((Coords)coordonates[j++]).Z);
+                GL.Vertex3(((Coords)coordonates[j]).X, ((Coords)coordonates[j]).Y, ((Coords)coordonates[j++]).Z);
+                GL.Vertex3(((Coords)coordonates[j]).X, ((Coords)coordonates[j]).Y, ((Coords)coordonates[j++]).Z);
+                GL.Vertex3(((Coords)coordonates[j]).X, ((Coords)coordonates[j]).Y, ((Coords)coordonates[j++]).Z);
+
+            }
+            GL.End();
+        }
+    }
+}
+
